4ee5c959a3b414278127118788d567bd3acea02c
Casian <57826342+CasianJors@users.noreply.github.com>
2025-08-07 19:47:32 +0300

    initial commit

---
+++ M1 - Cisco Essentials 1/lesson1 Introduction/main.py
@@ -0,0 +1,22 @@
+
+# comentariu
+
+"""
+
+comentariu
+
+"""
+
+nume = input()   # citire din consola
+
+print("Buna ziua", nume)  # afisare in consola
+
+a = 5
+b = 10
+suma = a+b    #suma intre 2 numere de tip int
+
+print(suma)
+
+
+
+
